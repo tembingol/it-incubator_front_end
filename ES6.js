392cbd399cbde49250caa0adec67ed02936ff12c
tembingol <tembingol@gmail.com>
2022-07-01 15:33:08 +0600

    feat: freeCodeCamp task #2 done

--- ES6.js
+++ ES6.js
@@ -1,11 +1,25 @@
 /*task 1 -->*/
 function checkScope() {
-    let i = 'function scope';
-    if (true) {
-      let i = 'block scope';
-      console.log('Block scope i is: ', i);
-    }
-    console.log('Function scope i is: ', i);
-    return i;
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
   }
-  /*task 1 --<*/
+  console.log("Function scope i is: ", i);
+  return i;
+}
+/*task 1 --<*/
+
+/*task 2 -->*/
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+  // Using s = [2, 5, 7] would be invalid
+
+  // Only change code above this line
+}
+editInPlace();
+/*task 2 --<*/

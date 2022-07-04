5051fdcd1344489788ec4d2bec05baddcb88116f
tembingol <tembingol@gmail.com>
2022-07-04 20:10:31 +0600

    feat: freeCodeCamp task #10-20 done

--- ES6.js
+++ ES6.js
@@ -108,3 +108,145 @@ const { tomorrow: highTomorrow } = HIGH_TEMPERATURES;
 
 // Only change code above this line
 /*task 10 --<*/
+
+/*task 11 -->*/
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 },
+};
+
+// Only change code below this line
+
+const {
+  today: { low: lowToday, high: highToday },
+} = LOCAL_FORECAST;
+// Only change code above this line
+/*task 11 --<*/
+
+/*task 12 -->*/
+let a = 8,
+  b = 6;
+[a, b] = [b, a];
+// Only change code below this line
+/*task 12 --<*/
+
+/*task 13 -->*/
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function removeFirstTwo(list) {
+  // Only change code below this line
+  const [, , ...arr] = list; // Change this line
+  // Only change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
+/*task 13 --<*/
+
+/*task 14 -->*/
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85,
+};
+
+// Only change code below this line
+//const half = (stats) => (stats.max + stats.min) / 2.0;
+const half = ({ max, min }) => {
+  return (max + min) / 2.0;
+};
+// Only change code above this line
+/*task 14 --<*/
+
+/*task 15 -->*/
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"],
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = [];
+  for (let i = 0; i < arr.length; i++) {
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+  }
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+/*task 15 --<*/
+
+/*task 16 -->*/
+const createPerson = (name, age, gender) => {
+  // Only change code below this line
+  return {
+    name,
+    age,
+    gender,
+  };
+  // Only change code above this line
+};
+/*task 16 --<*/
+
+/*task 17 -->*/
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  },
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
+/*task 17 --<*/
+
+/*task 18 -->*/
+// Only change code below this line
+class Vegetable {
+  constructor(name) {
+    this.name = name;
+  }
+}
+// Only change code above this line
+
+const carrot = new Vegetable("carrot");
+console.log(carrot.name); // Should display 'carrot'
+/*task 18 --<*/
+
+/*task 19 -->*/
+// Only change code below this line
+class Thermostat {
+  constructor(temp) {
+    this._temperature = temp;
+  }
+  // getter
+  get temperature() {
+    return (5 / 9) * (this._temperature - 32);
+  }
+  // setter
+  set temperature(temp) {
+    this._temperature = (temp * 9.0) / 5 + 32;
+  }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
+/*task 19 --<*/
+
+/*task 20 -->*/
+<html>
+  <body>
+    <!-- Only change code below this line -->
+<script type="module" src="index.js"></script>
+    <!-- Only change code above this line -->
+  </body>
+</html>
+/*task 20 --<*/
